c09603a55d30e6aa187ba7d3e2f7834f8ee89e3f
Andrew McMillan <andrew@gamesheetinc.com>
2025-01-03 12:39:42 -0800

    added transactions page and icons

--- src/components/mobile-menu.tsx
+++ src/components/mobile-menu.tsx
@@ -1,17 +1,26 @@
+import { Calendar1, FileText } from "lucide-react";
 import Link from "next/link";
 
 export default function MobileMenu() {
 	return (
 		<div className="absolute bottom-0 border-t-2 border-gray-500 w-full flex justify-center items-center bg-white">
-			<nav className="flex flex-col gap-4 w-full">
+			<nav className="flex flex-col gap-4 w-full py-4">
 				<ul className="flex w-full gap-4 justify-evenly">
 					<li>
-						<Link href="/transactions">
+						<Link
+							href="/transactions"
+							className="flex justify center items-center flex-col"
+						>
+							<FileText />
 							<span>Transactions</span>
 						</Link>
 					</li>
 					<li>
-						<Link href="/schedule">
+						<Link
+							href="/schedule"
+							className="flex justify center items-center flex-col"
+						>
+							<Calendar1 />
 							<span>Schedule</span>
 						</Link>
 					</li>
