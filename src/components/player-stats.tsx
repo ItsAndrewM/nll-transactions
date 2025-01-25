162530300e4601b8510c8a46b2a7ed84eab99f4d
Andrew McMillan <andrew@gamesheetinc.com>
2025-01-25 12:10:00 -0800

    add sorting functionality to PlayerStats table with sortable headers for better data management and user interaction

--- src/components/player-stats.tsx
+++ src/components/player-stats.tsx
@@ -6,6 +6,8 @@ import {
 	type ColumnDef,
 	flexRender,
 	getCoreRowModel,
+	getSortedRowModel,
+	SortingState,
 	useReactTable,
 } from "@tanstack/react-table";
 import {
@@ -21,26 +23,421 @@ import {
 	PlayerStats as PlayerStatsType,
 	RunnerStat,
 } from "@/types/games";
+import { Button } from "./ui/button";
+import { ArrowDown, ArrowUp, ArrowUpDown } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 const runnerColumns: ColumnDef<RunnerStat>[] = [
-	{ accessorKey: "number", header: "#" },
-	{ accessorKey: "name", header: "Name" },
-	{ accessorKey: "g", header: "G" },
-	{ accessorKey: "a", header: "A" },
-	{ accessorKey: "pts", header: "PTS" },
-	{ accessorKey: "sog", header: "SOG" },
-	{ accessorKey: "lb", header: "LB" },
-	{ accessorKey: "to", header: "TO" },
-	{ accessorKey: "cto", header: "CTO" },
-	{ accessorKey: "pim", header: "PIM" },
+	{
+		accessorKey: "number",
+		header: ({ column }) => {
+			console.log(column.getIsSorted());
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					#
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-6">{row.getValue("number")}</div>,
+	},
+	{
+		accessorKey: "name",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					Name
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("name")}</div>,
+	},
+	{
+		accessorKey: "g",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					G
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("g")}</div>,
+	},
+	{
+		accessorKey: "a",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					A
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("a")}</div>,
+	},
+	{
+		accessorKey: "pts",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					PTS
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("pts")}</div>,
+	},
+	{
+		accessorKey: "sog",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					SOG
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("sog")}</div>,
+	},
+	{
+		accessorKey: "lb",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					LB
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("lb")}</div>,
+	},
+	{
+		accessorKey: "to",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					TO
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("to")}</div>,
+	},
+	{
+		accessorKey: "cto",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					CTO
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("cto")}</div>,
+	},
+	{
+		accessorKey: "pim",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					PIM
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("pim")}</div>,
+	},
 ];
 
 const goalieColumns: ColumnDef<GoalieStat>[] = [
-	{ accessorKey: "number", header: "#" },
-	{ accessorKey: "name", header: "Name" },
-	{ accessorKey: "min", header: "MIN" },
-	{ accessorKey: "ga", header: "GA" },
-	{ accessorKey: "saves", header: "Saves" },
+	{
+		accessorKey: "number",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					#
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("number")}</div>,
+	},
+	{
+		accessorKey: "name",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					Name
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("name")}</div>,
+	},
+	{
+		accessorKey: "min",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					MIN
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("min")}</div>,
+	},
+	{
+		accessorKey: "ga",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					GA
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("ga")}</div>,
+	},
+	{
+		accessorKey: "saves",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					className={cn(
+						column.getIsSorted() !== false
+							? "font-medium text-black bg-accent"
+							: null,
+						"flex items-center gap-2 justify-start text-left"
+					)}
+				>
+					SAVES
+					{column.getIsSorted() === false ? (
+						<ArrowUpDown />
+					) : column.getIsSorted() === "asc" ? (
+						<ArrowUp />
+					) : (
+						<ArrowDown />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => <div className="pl-4">{row.getValue("saves")}</div>,
+	},
 ];
 
 function DataTable<TData>({
@@ -50,10 +447,16 @@ function DataTable<TData>({
 	columns: ColumnDef<TData>[];
 	data: TData[];
 }) {
+	const [sorting, setSorting] = useState<SortingState>([]);
 	const table = useReactTable({
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		onSortingChange: setSorting,
+		state: {
+			sorting,
+		},
 	});
 
 	return (
@@ -83,7 +486,7 @@ function DataTable<TData>({
 								data-state={row.getIsSelected() && "selected"}
 							>
 								{row.getVisibleCells().map((cell) => (
-									<TableCell key={cell.id}>
+									<TableCell key={cell.id} className="">
 										{flexRender(cell.column.columnDef.cell, cell.getContext())}
 									</TableCell>
 								))}
