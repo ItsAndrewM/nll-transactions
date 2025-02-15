654b0947bb0906180b120b68771307a636413775
Andrew McMillan <andrew@gamesheetinc.com>
2025-02-15 13:21:58 -0800

    fix: update player URL in sitemap to use personId for accurate mapping and ensure correct player identification in sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -62,7 +62,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 			priority: 0.5,
 		},
 		...players?.players.map((player: Player) => ({
-			url: `https://nlltracker.com/players/${player.id}`,
+			url: `https://nlltracker.com/players/${player.personId}`,
 			lastModified: new Date(),
 			changeFrequency: "weekly",
 			priority: 0.5,
